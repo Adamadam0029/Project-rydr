5bb6b686cce82f9238021b6027280111e9a805bd
Adamadam0029 <9028623@student.zadkine.nl>
2026-04-08 11:25:54 +0200

    aanbod pagina

--- pages/ons-aanbod.php
+++ pages/ons-aanbod.php
@@ -1,4 +1,11 @@
-<?php require "includes/header.php" ?>
+<?php
+require "includes/header.php";
+require_once "database/connection.php";
+$query = $conn->prepare("SELECT * FROM auto");
+$query->execute();
+$autos = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+ 
  
 <main>
     <h2>Ons aanbod</h2>
@@ -29,33 +36,52 @@
         <input type="range" min="o" max="30000">
 </aside>
  
-<section class="car-list">
-    <?php
-            $query = $conn->prepare("select * FROM auto");
-            $query ->execute();
-            $auto = $query->fetchAll(PDO::FETCH_ASSOC);
-            ?>
-             <?php foreach ($auto as $row): ?>
+             <section class= "content">
+      <section class="content">
+    <h2 class="section-title">Aanbevolen auto's</h2>
+ 
+    <div class="cars">
+        <?php foreach ($autos as $auto): ?>
             <div class="car-details">
+ 
                 <div class="car-brand">
-                    <h3><?= $row['merk'] ?></h3>
+                    <h3><?= $auto['merk'] ?> <?= $auto['model'] ?></h3>
                     <div class="car-type">
-                        <?= $row['model'] ?>
+                        <?= $auto['type'] ?>
                     </div>
-                </div>  
-                <img src="assets/images/products/<?= $row['merk'] ?>.jpg" alt="">
-   
+               
+ 
+                <img src="assets/images/<?= strtolower($auto['merk']) ?>.png" alt="">
+ 
                 <div class="car-specification">
-                    <span><img src="assets/images/icons/gas-station.svg" alt=""><?= $row['tankinhoud'] ?></span>
-                    <span><img src="assets/images/icons/car.svg" alt=""><?= $row['bestuurssysteem'] ?></span>
-                    <span><img src="assets/images/icons/profile-2user.svg" alt=""><?= $row['capaciteit'] ?></span>
+                    <span>
+                        <img src="assets/images/icons/gas-station.svg" alt="">
+                        <?= $auto['tankinhoud'] ?>
+                    </span>
+ 
+                    <span>
+                        <img src="assets/images/icons/car.svg" alt="">
+                        <?= $auto['bestuurssysteem'] ?>
+                    </span>
+ 
+                    <span>
+                        <img src="assets/images/icons/profile-2user.svg" alt="">
+                        <?= $auto['capaciteit'] ?>
+                    </span>
                 </div>
+ 
                 <div class="rent-details">
-                    <span><b>€<?= $row['prijs'] ?></b> / dag</span>
-                    <a href="/car-detail" class="button-primary">Bekijk nu</a>
+                    <span>
+                        <span class="font-weight-bold">€<?= $auto['prijs'] ?></span> / dag
+                    </span>
+                    <a href="#" class="button-primary">Bekijk nu</a>
                 </div>
+ 
             </div>
         <?php endforeach; ?>
+    </div>
+</section>
+               
              </section>
              </main>
 <?php require "includes/footer.php" ?>
