50d05ae160f6b50b5e26e5fc613d20aa9e442e43
Adamadam0029 <9028623@student.zadkine.nl>
2026-04-10 11:30:39 +0200

    het team pagina verbeterd

--- pages/over-ons.php
+++ pages/over-ons.php
@@ -1,17 +1,25 @@
 <?php require "includes/header.php" ?>
 <main>
+    <div class="hq">
     <img src="/assets/images/banner.jpeg" alt="" width="1200">
-    <h2>Over Rydr.</h2>
-    <div class="grid">
+</div>
+    <h2 class="overrydr">Over Rydr.</h2>
+    <div>
+        <h4>
         <div class="row">
-            <p>Ons hoofdkantoor bevindt zich in het bruisende hart van Rotterdam, direct naast het Centraal Station.
-                Hier combineren we technologie, design en klantgerichtheid onder één dak.</p>
+            <p>RYDR is gebouwd op visie, strategie en vertrouwen. Als CEO's staan wij elke dag voor één doel: het creëren van een 
+                platform dat de toekomst van mobiliteit opnieuw definieert. Professioneel. Schaalbaar. En altijd één stap vooruit</p>
             <p> In een modern pand met uitzicht op de skyline werken we elke dag aan de mobiliteit van morgen. Loop je
                 een keer binnen? De koffie staat klaar.</p>
         </div>
+        </h4>
+        <section class="ceo">
         <div class="row">
-            <img src="assets/images/work-place.png" alt="" width="400">
+            <img src="assets/images/ceo.png" alt="" width="400">
+            CEO's of <span>Rydr.</span>
+            <h5> Adam Ouchan & Youssef Meziane </h5>
         </div>
+        </section>
     </div>
 
 </main>
