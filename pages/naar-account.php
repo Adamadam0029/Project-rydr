3fcff1eab1f4c404c262b332d31aec47bb38980f
Adamadam0029 <9028623@student.zadkine.nl>
2026-04-10 11:29:49 +0200

    account pagina aangemaakt

---
+++ pages/naar-account.php
@@ -0,0 +1,14 @@
+<?php require "includes/header.php" ?>
+    <main>
+            <div>
+                <h2 class="overrydr">Uw <span>Rydr.</span> account</h2>
+            </div>
+        <section class="account1">
+            <div class="gegevens1"> e-mail</div>
+            <div class="gegevens"> voorbeeld e-mail</div>
+            <div class="gegevens1"> wachtwoord</div>
+            <div class="gegevens"> *********</div>
+            <a><span>Wachtwoord vergeten?</span></a>
+        </section>
+    </main>
+<?php require "includes/footer.php" ?>
